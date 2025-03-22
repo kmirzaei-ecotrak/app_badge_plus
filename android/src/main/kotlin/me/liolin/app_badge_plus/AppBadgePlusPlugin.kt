f455d5200d52160a13bf4ec41a1f6afeb35c478c
Kaveh Mirzaei <96082639+kmirzaei-ecotrak@users.noreply.github.com>
2025-03-22 09:23:23 -0700

    fix the test

--- android/src/main/kotlin/me/liolin/app_badge_plus/AppBadgePlusPlugin.kt
+++ android/src/main/kotlin/me/liolin/app_badge_plus/AppBadgePlusPlugin.kt
@@ -1,51 +1,44 @@
 package me.liolin.app_badge_plus
 
 import android.content.Context
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import me.liolin.app_badge_plus.badge.Badge
+import me.liolin.app_badge_plus.badge.BadgeUtil
 
+class AppBadgePlusPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
+  private lateinit var context: Context
 
-/** AppBadgePlusPlugin */
-class AppBadgePlusPlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
-    private var context: Context? = null
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "app_badge_plus")
+    channel.setMethodCallHandler(this)
+    context = flutterPluginBinding.applicationContext
+  }
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "app_badge_plus")
-        channel.setMethodCallHandler(this)
-        context = flutterPluginBinding.applicationContext
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+      "updateBadge" -> {
+        val count = call.argument<Int>("count")!!
+        BadgeUtil.update(context, count)
+        result.success(null)
+      }
+      "isSupported" -> {
+        result.success(BadgeUtil.isSupported(context))
+      }
+      "getPlatformVersion" -> {
+        result.success("Android " + android.os.Build.VERSION.RELEASE)
+      }
+      else -> {
+        result.notImplemented()
+      }
     }
+  }
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when (call.method) {
-            "updateBadge" -> {
-                val args = call.arguments as Map<*, *>
-                val count: Int = args["count"] as Int
-                context?.let { Badge.updateBadge(it, count) }
-                result.success(null)
-            }
-
-            "isSupported" -> {
-                val isSupported = context?.let { Badge.isBadgeSupported(it) } ?: false
-                result.success(isSupported)
-            }
-
-            else -> {
-                result.notImplemented()
-            }
-        }
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-        context = null
-    }
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 }
